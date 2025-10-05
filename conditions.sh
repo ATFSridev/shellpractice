b98415b10c76cf6e257aca76d9d4ee9c41aaa32a
unknown <testofb.sridev@gmail.com>
2025-10-04 19:14:30 -0700

    update

--- conditions.sh
+++ conditions.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ];then
 	echo "please run the script with root ID"
+	exit 1  #failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
 	echo "error ::: Installing failure"
+	exit 1
 else
 	echo " mysql installed ....."
 
