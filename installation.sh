3fb46e71189fde650676dc81726feb8dd160b985
unknown <testofb.sridev@gmail.com>
2025-10-04 19:28:28 -0700

    update

---
+++ installation.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#install mongoDB, nginx mysql
+
+USER=$(id -u)
+
+if [ $USER ne 0 ] ; then
+	echo "not a root user"
+	exit 1
+
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+	echo "ERROR : insatlling mysql is failed"
+	exit 1
+else
+	echo "installed mysql"
+
+fi
+
+
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+        echo "ERROR : insatlling nginx is failed"
+        exit 1
+else
+        echo "installed nginx"
+
+fi
+dnf install python3 -y
+
+
+if [ $? -ne 0 ]; then
+        echo "ERROR : insatlling mysql is failed"
+        exit 1
+else
+        echo "installed  python"
+
+fi
+
+
+
+
+
